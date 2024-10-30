2bc14b0f2418c6cc2aad8970df36013142557b9a
Mithun Mohan A <85548289+mithunmohan9633@users.noreply.github.com>
2024-10-30 13:46:44 +0530

    Delete car_project directory

--- car_project/listings/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ListingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'listings'
-    def ready(self):
-        pass
